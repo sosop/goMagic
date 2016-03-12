ee0ac095cce129b401b0518292890930e2a8937d
侯小龙 <houxiaolong@yunyao-inc.com>
2016-03-12 17:09:54 +0800

    del bench

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -5,7 +5,21 @@ import (
 	"testing"
 )
 
+var bq BlockQueue
+
 func TestMain(m *testing.M) {
+	bq = NewMemQueue()
 	status := m.Run()
 	os.Exit(status)
+}
+
+func TestLength(t *testing.T) {
+	bq.Push(1)
+	if bq.Length() != 1 {
+		t.Fatal("push error")
+	}
+	bq.Pop()
+	if bq.Length() != 0 {
+		t.Fatal("pop error")
+	}
 }
