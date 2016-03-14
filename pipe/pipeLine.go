7df2ea40cb369aa9052343d28eee79a2056d301f
侯小龙 <houxiaolong@yunyao-inc.com>
2016-03-14 13:28:42 +0800

    add file pipe and test

--- pipe/pipeLine.go
+++ pipe/pipeLine.go
@@ -26,3 +26,24 @@ func (console *ConsolePipeline) Out(s string) error {
 func (console *ConsolePipeline) Close() error {
 	return console.w.Close()
 }
+
+type FilePipeline struct {
+	w *os.File
+}
+
+func NewFilePipeline(filename string) *FilePipeline {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	return &FilePipeline{file}
+}
+
+func (filePipeline *FilePipeline) Out(s string) error {
+	_, err := filePipeline.w.WriteString(s)
+	return err
+}
+
+func (filePipeline *FilePipeline) Close() error {
+	return filePipeline.w.Close()
+}
--- pipe/pipeline_test.go
+++ pipe/pipeline_test.go
@@ -2,12 +2,18 @@ package pipe
 
 import "testing"
 
-var pipeline Pipeline
-
 func TestConsolePipelineOut(t *testing.T) {
-	pipeline = NewConsolePipeline()
+	pipeline := NewConsolePipeline()
 	defer pipeline.Close()
 	if err := pipeline.Out("testing console"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestFilePipelineOut(t *testing.T) {
+	pipeline := NewFilePipeline("/Users/mac/filePipeline")
+	defer pipeline.Close()
+	if err := pipeline.Out("testing file"); err != nil {
 		t.Fatal(err)
 	}
 }
